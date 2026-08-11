7e8dc9ec6b20b0ecb7c172866c4e2099570817dc
Palak Banik <polokbanik7@gmail.com>
2026-08-11 19:55:21 +0600

    learn environment variables & make create, read, update, delete methods

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -44,24 +44,23 @@ handler.handleReqRes = (req, res) => {
         ? routes[trimmedPath]
         : notFoundHandler;
 
-    chosenHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof statusCode === "number" ? statusCode : 500;
-        payload = typeof payload === "object" ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-
-        // return the final result
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    });
-
     req.on("data", (buffer) => {
         realData += decoder.write(buffer);
     });
 
     req.on("end", () => {
         realData += decoder.end();
-        console.log(realData);
+
+        chosenHandler(requestProperties, (statusCode, payload) => {
+            statusCode = typeof statusCode === "number" ? statusCode : 500;
+            payload = typeof payload === "object" ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the final result
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
 
         // response handle
         res.end("Hello Programmers!");
